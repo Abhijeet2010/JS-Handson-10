b171d2ccafb0745e3154bae6ab66883a694ca04b
Abhijeet2010 <abhijeetraut220@gmail.com>
2022-12-15 16:27:01 +0530

    completed JS-Handson-10 (Functions)

---
+++ index.js
@@ -0,0 +1,27 @@
+// Q.1
+// const zeroPara = ()=>{
+//     console.log("hello")
+// }
+// zeroPara()
+
+// Q.2
+
+
+// const sum = (num1, num2)=>{
+//      console.log(num1+num2)
+// }
+// sum(10,20)
+
+// 
+
+
+
+var x = 21;
+
+function girl (){
+    console.log(x);
+    var x = 20;
+};
+
+
+
